76f64baeda6a885850eca794eaa97268d531301e
LDG <dheornjf@naver.com>
2024-07-22 12:15:47 +0900

    로그인 조건, 회원가입, 로그아웃 기능 작성

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,42 +4,70 @@ const conn = require('../config/db')
 
 // 회원가입
 router.post('/join', (req, res) => {
-    console.log('join 실행', req.body)
+    console.log('join 실행', req.body);
 
-    let { nick, pw, btd, gen } = req.body
+    let { nick, pw, btd, gen } = req.body;
 
-    let sql = "insert into KIOSK_USER_TB values (?, ?, ?, ?)"
-    conn.query(sql, [nick, pw, btd, gen], (err, rows) => {
-        console.log(err);
-        console.log('insert 완료', rows)
-        if (rows) {
-            res.redirect('/')
-        } else {
-            res.send(`<script>
-                        alert('다시 시도해주세요')
-                        window.location.href='/join'
-                    </script>`)
+    if (!nick || !pw) {
+        return res.send("<script>alert('별명과 비밀번호를 입력해주세요'); window.history.back();</script>");
+    }
+
+    let checkNickSql = "SELECT * FROM KIOSK_USER_TB WHERE USER_NICK = ?";
+    conn.query(checkNickSql, [nick], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.send("<script>alert('오류가 발생했습니다. 다시 시도해주세요.'); window.history.back();</script>");
         }
 
-    })
+        if (results.length > 0) {
+            return res.send("<script>alert('이미 존재하는 별명입니다'); window.history.back();</script>");
+        }
 
-})
+        let sql = "INSERT INTO KIOSK_USER_TB VALUES (?, ?, ?, ?)";
+        conn.query(sql, [nick, pw, btd, gen], (err, rows) => {
+            if (err) {
+                console.error(err);
+                return res.send("<script>alert('오류가 발생했습니다. 다시 시도해주세요.'); window.history.back();</script>");
+            }
+
+            console.log('insert 완료', rows);
+            res.redirect('/');
+        });
+    });
+});
 
 // 로그인
 router.post('/login', (req, res) => {
-    console.log('login', req.body)
-    let { userNick, userPw } = req.body
+    console.log('login', req.body);
+    let { nick, pw } = req.body;
 
-    let sql = 'SELECT USER_NICK FROM KIOSK_USER_TB WHERE USER_NICK = ? AND USER_PW = ?'
-    conn.query(sql, [userNick, userPw], (err, rows) => {
-        console.log('rows', rows)
-        if (rows.length > 0) {
-            req.session.nick = userNick;
-            res.redirect('/')
-        } else {
-            res.send('<script>("아이디 혹은 비밀번호를 잘못입력하셨습니다.")</script>')
+    // 입력 데이터 검증
+    if (!nick || !pw) {
+        return res.send('<script>alert("아이디와 비밀번호를 입력해주세요."); window.history.back();</script>');
+    }
+
+    let sql = 'SELECT USER_NICK, USER_PW FROM KIOSK_USER_TB WHERE USER_NICK = ? AND USER_PW = ?';
+    conn.query(sql, [nick, pw], (err, rows) => {
+        if (err) {
+            console.error(err);
+            return res.send('<script>alert("오류가 발생했습니다. 다시 시도해주세요."); window.history.back();</script>');
         }
-    })
+
+        console.log('rows', rows);
+        if (rows.length > 0) {
+            req.session.nick = nick;
+            res.redirect('/');
+        } else {
+            res.send('<script>alert("아이디 혹은 비밀번호를 잘못 입력하셨습니다."); window.history.back();</script>');
+        }
+    });
+});
+
+// 로그아웃 (<script>문 사용시 ;을 제대로 적어주지 않으면 기능 수행되지 않음)
+router.get('/logout', (req, res) => {
+    console.log('로그아웃')
+    req.session.destroy()
+    res.send('<script>window.location.href="/";</script>');
 })
 
 // 회원정보 수정
@@ -55,8 +83,7 @@ router.post('/update', (req, res) => {
         } else {
             res.send(`<script>
                 alert("별명 혹은 비밀번호를 다시 입력해주세요.")
-                window.location.href="/update"
-                </script>`)
+                window.location.href="/update"</script>`)
         }
     })
 })
